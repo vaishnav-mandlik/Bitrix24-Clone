be9b5818683a3cb703b3c5f4ac9206ffdcc54b7a
vaishanv-mandlik <vaishnavmandlik@yahoo.com>
2022-09-06 13:31:12 +0530

    feed page

---
+++ stream.js
@@ -0,0 +1,27 @@
+let clock = document.getElementById("clock");
+function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+
+  let session = "AM";
+
+  if (hh == 0) {
+    hh = 12;
+  }
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
+
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+
+  let time = hh + ":" + mm + " " + session;
+  clock.innerText = time;
+  console.log(time);
+}
+currentTime();
+let t = setTimeout(function () {
+  currentTime();
+}, 60000);
